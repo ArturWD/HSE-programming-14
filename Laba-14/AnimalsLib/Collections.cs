0863ab27ddbec6a1199ddb4aefd36ba781af2f03
ArturWD <shestakovartur2.0@gmail.com>
2018-11-01 14:03:33 +0500

    Changed collection class

---
+++ Laba-14/AnimalsLib/Collections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsLib
+{
+    public class Collections<T>
+    {
+        public List<T> zoo { get; set; }
+        public List<T> birdSection { get; set; }
+        public Collections()
+        {
+            zoo = new List<T>();
+            birdSection = new List<T>(0);
+        }
+    }
+}
